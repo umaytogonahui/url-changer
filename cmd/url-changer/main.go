833a5040564d80d288e9d173341d406f2ae179c1
Romanian Cadillac <mm.tbde@gmail.com>
2023-09-15 23:02:04 +0300

    1day commit

---
+++ cmd/url-changer/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//TO DO: init config: cleanenv
+
+	//TO DO: init logger: slog
+
+	//TO DO: init storage: sqlite
+
+	//TO DO: init router: chi, "chi ren"
+
+	//TO DO: run server
+}
